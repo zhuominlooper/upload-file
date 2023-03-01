81df2ca93efe195ea0830fbd337908efb53b9f3e
looper <2390903545@qq.com>
2023-03-01 15:11:26 +0800

    优化处理细节

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # upload-file
-实现文件的 BASE64 和 FROMDATA 的格式上传，以及大文件的分片和断点续传功能,该项目包含前端后 node 后端，所以需要安装前后端的依赖  
+
+实现文件的 BASE64 和 FROMDATA 的格式上传，以及大文件的分片和断点续传功能,该项目包含前端后 node 后端，所以需要安装前后端的依赖
 
 ![RUNOOB 图标](https://p1-jj.byteimg.com/tos-cn-i-t2oaga2asx/gold-user-assets/2020/5/13/1720b816a2303d87~tplv-t2oaga2asx-zoom-crop-mark:1304:1304:1304:734.awebp)
-
 
 ## 客户端
 
@@ -14,7 +14,7 @@
 
 1. cd ./server
 2. npm i or yarn 安装前端依赖
-3. npm run start 启动服务器
+3. node server.js 启动服务器
 
 ## 上传的功能
 
@@ -29,8 +29,8 @@
 ## 大文件上传
 
 ### 注意点
+
 1. 分片的策略，控制分片数量和分片大小
 2. 分片的上传并发需要控制，不然会造成服务器和浏览器压力过大
-3. 分片的文件名很重要，提供了smark-md5根据内容生成hash值
+3. 分片的文件名很重要，提供了 smark-md5 根据内容生成 hash 值
 4. 判断切片上传完成的时机，进行分片的合并
-     
--- client/src/components/chunk-upload.vue
+++ client/src/components/chunk-upload.vue
@@ -2,7 +2,7 @@
   <div class="main">
     <input
       v-show="false"
-      id="file-upload"
+      ref="file-upload"
       type="file"
       class="upload_inp"
       accept="*"
@@ -23,6 +23,7 @@
         >文件大小:<span>{{ fileData.size }}b</span></el-col
       >
     </el-row>
+    <br />
     上传进度:<el-progress :percentage="processData"></el-progress>
     <br />
     <el-link disabled>可以传任意格式的文件，支持大文件上传</el-link>
@@ -42,11 +43,11 @@ export default {
       fileData: null, //保存文件
       processData: 0,
       alreadUploadFileCount: 0,
-      alreadUploadFile:[] //保存已经上传的文件
+      alreadUploadFile: [], //保存已经上传的文件
     };
   },
   mounted() {
-    this.inputInstance = document.getElementById("file-upload");
+    this.inputInstance = this.$refs["file-upload"];
     //获取选择文件的监听
     this.monitorChooseUploadFile();
   },
@@ -85,12 +86,13 @@ export default {
     //上传文件
     async uploadFile() {
       let { HashData, suffix } = await this.getHashName(this.fileData);
-
+      console.log(HashData, suffix);
       //获取服务器有没有该文件的断点分片
-       this.alreadUploadFile = (await this.getChunkData(HashData)).fileList || [];
+      this.alreadUploadFile =
+        (await this.getChunkData(HashData)).fileList || [];
       this.alreadUploadFileCount = this.alreadUploadFile.length;
       //1.确定分片策略,拿到分片的数量和分片大小
-      let maxChunkDataSize = 1024 * 100; //默认最大的分片大小
+      let maxChunkDataSize = 1024 * 100; //默认最大的分片大小(100k)
       let chunkCount = Math.ceil(this.fileData.size / maxChunkDataSize); //向上取值获取分片数量
       //如果分片超过100，就可以用100个分片来重新算分片大小
       if (chunkCount > 100) {
@@ -118,65 +120,63 @@ export default {
 
       //3. 进行分片的上传，采用promise all的原理来实现
       this.uploadChunkFile(chunkFileData, chunkCount)
-        .then(() => {
+        .then((res) => {
           //4. 对分片进行合并
+          this.$message.success(res);
           this.mergeChunkFile(HashData, chunkCount);
         })
         .catch((err) => {
           console.error("分片上传错误:", err);
         });
     },
-    
+
     uploadChunkFile(chunkFile, chunkCount) {
       let self = this;
       let index = 0;
       let isComplete = (resolve) => {
         index++;
         if (index === chunkCount) {
-          resolve("分片上传成功");
+          resolve("分片上传完成");
         }
       };
       return new Promise((resolve, reject) => {
         let maxCount = 10; //最大并发数量
         let promisePool = []; //并发请求的容器
-        let isError = false; //防止上传错误时其他分片继续上传
+
         while (promisePool.length < maxCount) {
           promiseTask(chunkFile.shift(), chunkFile);
         }
-        function promiseTask(chunkFileData, chunkFile) {
-          if (isError) {
-            return;
-          }
-          if (!self.alreadUploadFile.includes(chunkFileData.filename)) {
-          let fm = new FormData();
-          fm.append("file", chunkFileData.file);
-          fm.append("filename", chunkFileData.filename);
-          let pengdingPromise = instance.post("/upload_chunk", fm);
-          promisePool.push(pengdingPromise);
-          pengdingPromise
-            .then(() => {
-              promisePool.splice(promisePool.indexOf(pengdingPromise), 1);
-              self.processData = parseInt(
-                (self.alreadUploadFileCount++ / chunkCount) * 100
-              );
-              isComplete(resolve);
-              let data = chunkFile.shift();
-              if (data) {
-                promiseTask(data, chunkFile);
-              }
-            })
-            .catch((err) => {
-              reject(err);
-              isError = true;
-            });
-           }else{
-             isComplete(resolve);
-           }
 
+        function promiseTask(chunkFileData, chunkFile) {
+          if (!self.alreadUploadFile.includes(chunkFileData.filename)) {
+            let fm = new FormData();
+            fm.append("file", chunkFileData.file);
+            fm.append("filename", chunkFileData.filename);
+            let pengdingPromise = instance.post("/upload_chunk", fm);
+            promisePool.push(pengdingPromise);
+            pengdingPromise
+              .then(() => {
+                promisePool.splice(promisePool.indexOf(pengdingPromise), 1);
+                self.processData = parseInt(
+                  (self.alreadUploadFileCount++ / chunkCount) * 100
+                );
+                isComplete(resolve);
+                let data = chunkFile.shift();
+                if (data) {
+                  promiseTask(data, chunkFile);
+                }
+              })
+              .catch((err) => {
+                reject(err);
+              });
+          } else {
+            isComplete(resolve);
+          }
         }
       });
     },
 
+    //进行合片
     mergeChunkFile(HashData, chunkCount) {
       instance
         .post(
@@ -193,7 +193,7 @@ export default {
         )
         .then(() => {
           this.processData = 100;
-          this.$message.info("上传成功");
+          this.$message.success("上传成功");
         })
         .catch((err) => {
           this.$message.error(err);
@@ -204,7 +204,8 @@ export default {
     monitorChooseUploadFile() {
       this.inputInstance.addEventListener("change", () => {
         let file = this.inputInstance.files[0];
-        if (!file) return;
+        if (!file || file.size < 1024 * 1024 * 5)
+          return this.$message.warning("请选择至少5m的文件");
         //获取到文件
         this.fileData = file;
       });
@@ -216,7 +217,7 @@ export default {
 <style scoped>
 .main {
   border: 1px dashed rgb(236, 236, 236);
-  width: 400px;
+  width: 500px;
   height: 200px;
   padding: 16px;
 }
